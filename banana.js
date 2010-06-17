5496dd2704c4e4600f7981a98cb4e10e75b0f272
digish.pandya <digish.pandya@b70b6b6c-88ab-d94b-4bac-ba91c6d6ed33>
2010-06-17 09:32:37 +0000

    separated banana scripts

--- banana.js
+++ banana.js
@@ -1,251 +1,7 @@
-var banana_timer = window.setTimeout('activity_tracker()',1000);
-var banana_state = "NOT_EATING";
-var banana_basket = new Array;
-
-var used_banana = 0;
-var banana_total;
-function banana()
-{
-  this.orig                 = new Array();
-  this.formated             = new Array();
-  this.status               = new Array();
-  this.awin;
-  this.bwin;
-  
-  this.ChangeDisp=ChangeDisp;
-  this.ChangeSize=ChangeSize;
-}
-function ChangeSize(factor)
-{
-  switch(factor)
-  {
-  case 1:
-    this.awin.style.width = '100%';
-    this.bwin.style.width = '100%';
-  break;
-  case 2:
-    this.awin.style.width = '49%';
-    this.bwin.style.width = '49%';
-  break;
-  case 3:
-    this.awin.style.width = '32%';
-    this.bwin.style.width = '32%';
-  break;
-  default:
-  break;
-  }
-}
-function ChangeDisp(screen,cmd)
-{
-  if (cmd == "on") {
-    if (screen=="a") {
-      this.awin.style.display = 'block';
-    }
-    else if (screen=="b") {
-      this.bwin.style.display = 'block';
-    }
-  } else {
-    if (screen=="a") {
-      this.awin.style.display = 'none';
-    }
-    else if (screen=="b") {
-      this.bwin.style.display = 'none';
-    }
-  }
-  return;
-}
-
-function activity_tracker() {
-
-    switch(banana_state) 
-    {
-     case "NOT_EATING":
-          break;
-     case "OPENING":
-          open_banana();
-          break;
-     case "EATING":
-          eat_banana_bite();
-          break;
-     case "ENJOYING":
-          enjoy_banana_bite();
-          break;
-     case "THROW_AWAY":
-          window.clearTimeout(banana_timer);
-          break;
-     break; 
-    }
-}
-
-function open_banana()
-{
-  while (used_banana < banana_total)
-  {
-    banana_basket[used_banana] = new banana();
-    banana_basket[used_banana].awin = document.getElementById('editor'+used_banana);
-    banana_basket[used_banana].bwin = document.getElementById('editor_pp1'+used_banana);
-    banana_basket[used_banana].orig =  banana_basket[used_banana].ewin.value.split('\n');
-    used_banana++;
-  }
-}
-function eat_banana_bite()
-{
-  if (banana_total < 2) {
-    return;
-  }
-  
-}
-function enjoy_banana_bite()
-{
-
-}
-
 function reset_stuff()
 {
   document.getElementById("file_load_b").value = "";
 }
-
-function compare2() {
-  files[0]      =   new cfile;
-  files[1]      =   new cfile;
-  files[0].orig =  (document.getElementById('editor1').value).split('\n');
-  files[1].orig =  (document.getElementById('editor2').value).split('\n');
-  
-  var i;
-  var j;
-  var k = 0;
-  var p = 0;
-  var matched=0;
-  var left="l";
-  var right="r";
-  
-  var ppl = document.getElementById('editor_pp1');
-  var ppr = document.getElementById('editor_pp2');
-  ppl.style.width = '49%';
-  ppr.style.width = '49%';
-  
-  var s1 = new Array();
-  var s2 = new Array();
-  
-  if (files[0].orig.length < files[1].orig.length)
-    {
-      i = files[0];
-      files[0] = files[1];
-      files[1] = i;
-      i = ppl;
-      ppl = ppr;
-      ppr = i;
-      
-      left="r";
-      right="l";
-      
-    }
-  
-  for (i=0; i < files[0].orig.length; i++)
-    {
-      j=k;
-      if (j >= files[1].orig.length) 
-    {
-      files[1].orig = files[1].orig.concat("");
-      files[1].status[i] = "a"+left; // Added for left
-      files[0].status[i] = "m"+right; // missing in right
-      j++;
-      continue;
-    }
-      
-      for ( ; j < files[1].orig.length; j++)
-    {
-      if (files[0].orig[i] == files[1].orig[j]) {
-                        
-        matched = 1;
-        
-        if (i != j)
-          {
-        
-        p = j-i;
-        for (; p > 0 ; p-- )
-          {
-            var slice_pre;
-            var slice_post;
-            slice_pre  = files[0].orig.slice(0,i);
-            slice_post = files[0].orig.slice(i,files[0].orig.length);
-            files[0].orig = slice_pre.concat("",slice_post);
-            files[0].status[i]="a"+right; // Added for right
-            files[1].status[i]="m"+left; // Missing in left
-            i++;
-          }
-        
-          }
-        k=j+1;
-        
-        files[0].status[i] = "n"+right;  // normal with right
-        files[1].status[i] = "n"+left; // normal with left
-        
-        break;
-      }
-      else if (1 == match_percentage(files[0].orig[i],files[1].orig[j],s1,s2))
-        {
-          matched = 1;
-          
-          if (i != j)
-        {
-          p = j-i;
-          for (; p > 0 ; p-- )
-            {
-              var slice_pre;
-              var slice_post;
-              slice_pre  = files[0].orig.slice(0,i);
-              slice_post = files[0].orig.slice(i,files[0].orig.length);
-              files[0].orig = slice_pre.concat("",slice_post);
-              files[0].status[i]="a"+right; // Added for right
-              files[1].status[i]="m"+left; // Missing in left
-              i++;
-            }
-          
-        }
-          k=j+1;
-          //files[0].status[i]="partm"+right; // partial match right
-          //files[1].status[i]="partm"+left; // partial match left
-          files[0].status[i]=s1; // partial match right
-          files[1].status[i]=s2; // partial match left
-          s1 = new Array();
-          s2 = new Array();
-          break;
-        }
-      
-    }
-      
-      if (matched == 1) 
-    {
-      matched = 0;
-    }
-      else
-    {
-      var slice_pre;
-      var slice_post;
-      slice_pre  = files[1].orig.slice(0,i);
-      slice_post = files[1].orig.slice(i,files[1].orig.length);
-      files[1].orig = slice_pre.concat("",slice_post);
-      files[1].status[i]="a"+left; // Added for left
-      files[0].status[i]="m"+right; // Missing in right
-      k++;
-    }
-    }
-  
-  ppl.innerHTML = "";
-  ppr.innerHTML = "";
-  
-  formated_print(ppl,ppr,files[0],files[1]);
-  
-  document.getElementById('editor1').style.display = 'none';
-  document.getElementById('editor2').style.display = 'none';
-  document.getElementById('editor3').style.display = 'none';
-  document.getElementById('editor_pp1').style.display = 'block';
-  document.getElementById('editor_pp2').style.display = 'block';
-  //          document.getElementById('editor_pp3').style.display = 'block';
-}
-
-
 
 var total_files = 0;
 function load()
@@ -522,7 +278,7 @@ function format_string(c,i)
 	  
 	}
       if (k) {
-	c.formated[i] =c.formated[i]+"<br />"
+	    c.formated[i] =c.formated[i]+"<br />"
 	  }
       break;
     }
@@ -548,21 +304,23 @@ var vPos1=ref1.scrollTop,
   vPos2=ref2.scrollTop,
   vPos3=ref3.scrollTop;
 
-function scrollsync() {
-  if(ref1.scrollTop!=vPos1) {
+function scrollsync() 
+{
+  if(ref1.scrollTop!=vPos1) 
+  {
     vPos1=ref3.scrollTop=ref2.scrollTop=ref1.scrollTop;
   }
-  
-  else if(ref2.scrollTop!=vPos2) {
+  else if(ref2.scrollTop!=vPos2) 
+  {
     vPos2=ref3.scrollTop=ref1.scrollTop=ref2.scrollTop;
   }
-  
-  else if (ref3.scrollTop!=vPos3) {
+  else if (ref3.scrollTop!=vPos3) 
+  {
     vPos3=ref1.scrollTop=ref2.scrollTop=ref3.scrollTop;
   }
   setTimeout('scrollsync()',1500)
     
-    }
+}
 
 
 window.onload=scrollsync;
